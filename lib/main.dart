7982af72f7a00f58cd9e29a0ddefeb62573a451f
YakerHuertas26 <123754558+YakerHuertas26@users.noreply.github.com>
2024-11-07 23:56:32 -0500

    botones

--- lib/main.dart
+++ lib/main.dart
@@ -30,27 +30,45 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     // retornar la estructura del appMóvil
     return Scaffold(
-        appBar: AppBar(
-          title: Text('MY NEW APP'),
-        ),
-        body: Column(
-          // alinear del contenido en su eje principal
-          mainAxisAlignment: MainAxisAlignment.center,
-          // alinea en su eje secundario
-          crossAxisAlignment: CrossAxisAlignment.start,
-          // definir el tamaño maximo y mínimo
-          mainAxisSize: MainAxisSize.max,
+      appBar: AppBar(
+        title: Text('MY NEW APP'),
+      ),
+      // body: Column(
+      //   // alinear del contenido en su eje principal
+      //   mainAxisAlignment: MainAxisAlignment.center,
+      //   // alinea en su eje secundario
+      //   crossAxisAlignment: CrossAxisAlignment.sat,
+      //   // definir el tamaño maximo y mínimo
+      //   mainAxisSize: MainAxisSize.max,
 
-          children: <Widget>[
-            Container(
-                width: MediaQuery.of(context).size.width,
-                child: Text(
-                  'text one 01',
-                  textAlign: TextAlign.center,
-                )),
-            Text('texto two 02'),
-            Text('texto 03')
-          ],
-        ));
+      //   children: <Widget>[
+      //     Container(
+      //         width: MediaQuery.of(context).size.width,
+      //         child: Text(
+      //           'text one 01',
+      //           textAlign: TextAlign.center,
+      //         )),
+      //     Text('texto two 02'),
+      //     Text('texto 03')
+      //   ],
+      // ));
+      body: Center(
+        child: ElevatedButton(
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Icon(Icons.access_alarms),
+              Text('now'),
+              SizedBox(
+                width: 8,
+              )
+            ],
+          ),
+          onPressed: () {
+            print('hola');
+          },
+        ),
+      ),
+    );
   }
 }
